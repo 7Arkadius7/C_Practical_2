74218cb8ab7e67023b61e20763b1a683597f04b8
Sergey <motor-person@yandex.ru>
2022-10-02 22:52:01 +0400

    Решили задачу Task_09

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -1,3 +1,15 @@
-﻿
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+int number = new Random().Next(10, 100);
+
+Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
+
+int firstDigit = number/10;
+int secondDigit = number % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Первое число {firstDigit}, больше второго числа {secondDigit}");
+}
+else
+ Console.WriteLine($"Второе число {secondDigit}, больше первого числа {firstDigit}");
