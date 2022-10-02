d758a30a2fcb5682596ffb285cb9baf395f19c65
Sergey <motor-person@yandex.ru>
2022-10-02 23:33:22 +0400

    Дополнили второй раз решение задачи 09

--- Task_09/Program.cs
+++ Task_09/Program.cs
@@ -3,10 +3,26 @@
 int number = new Random().Next(10, 100);
 
 Console.WriteLine($"Случайное число из отрезка 10-99 => {number}");
-
+Console.WriteLine();
 int firstDigit = number/10;
 int secondDigit = number % 10;
 if (firstDigit == secondDigit)
 Console.WriteLine("Числа равны");
 else if (firstDigit > secondDigit) Console.WriteLine($"Первое число {firstDigit}, больше второго числа {secondDigit}");
 else Console.WriteLine($"Второе число {secondDigit}, больше первого числа {firstDigit}");
+Console.WriteLine();
+int maxDigit = firstDigit > secondDigit ? firstDigit : secondDigit;
+Console.WriteLine($"Большее число {maxDigit}");
+
+int MaxDigit (int num)
+{
+int firstDigit = num/10;
+int secondDigit = num % 10;
+if (firstDigit == secondDigit) return 0;
+if (firstDigit > secondDigit) return firstDigit;
+return secondDigit;
+}
+
+int result = MaxDigit (number);
+Console.WriteLine();
+Console.WriteLine($"Большее число {result}");
