ed810e22589dd51d85713899b6f06dc7b0a3bebc
Sergey <motor-person@yandex.ru>
2022-10-03 20:20:48 +0400

    Сделали Task_12

---
+++ Task_12/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool IsAliqut(int arg1, int arg2)
+{
+    return arg1 % arg2 == 0;
+}
+
+if (IsAliqut (num1, num2)) Console.WriteLine($"{num1} кратно {num2}");
+else Console.WriteLine($"{num1} не кратно {num2}, остаток: {num1 % num2} ");
