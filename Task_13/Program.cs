2526497805cf750b05116a0f49304a1e3c2dc656
Sergey <motor-person@yandex.ru>
2022-10-04 01:51:27 +0400

    Решил Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100) Console.WriteLine($"Число {number} не имеет третьей цифры");
+else
+{
+    while (number > 1000)
+    {
+        number = number / 10;
+    }
+
+    int result = number % 10;
+    Console.WriteLine($"Число {result}");
+}
