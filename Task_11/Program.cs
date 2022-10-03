86e44be16f051c11177fc57612527912b19a81ab
Sergey <motor-person@yandex.ru>
2022-10-03 20:04:37 +0400

    Сделали Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+int number = new Random().Next(100, 1000);
+
+int RemooveSecondDigit(int num)
+{
+    int first = num / 100;
+    int third = num % 10;   
+    return first * 10 + third;
+}
+int result = RemooveSecondDigit(number);
+
+Console.WriteLine($"Случайное число - {number}");
+Console.WriteLine($"Итоговое число - {result}");
