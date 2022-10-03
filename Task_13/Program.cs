0ad6d3e8efceb918c647a47d8fa487877b430158
Sergey <motor-person@yandex.ru>
2022-10-04 01:58:03 +0400

    Решил Task_13 при помощи функции

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -3,14 +3,19 @@
 Console.WriteLine("Пожалуйста, введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
+int ThirdDigit(int num)
+{
+    while (num > 1000)
+    {
+        num = num / 10;
+    }
+    int res = num % 10;
+    return res;
+}
+
 if (number < 100) Console.WriteLine($"Число {number} не имеет третьей цифры");
 else
 {
-    while (number > 1000)
-    {
-        number = number / 10;
-    }
-
-    int result = number % 10;
+    int result = ThirdDigit(number);
     Console.WriteLine($"Число {result}");
 }
