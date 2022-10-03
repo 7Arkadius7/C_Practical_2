d433f9f7309fca373996aaabcad8853886a5012e
Sergey <motor-person@yandex.ru>
2022-10-04 00:51:54 +0400

    Сделали Task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SecondDigit(int num)
+    {
+        int arg = num / 10;
+        int arg2 = arg % 10;
+        return arg2;
+    }
+int secondDigit = SecondDigit(number);
+
+if (number > 99 && number < 1000) Console.WriteLine($"Вторая цыфра числа {number} равна {secondDigit}");
+else Console.WriteLine("Вы ввели не корректное число");
