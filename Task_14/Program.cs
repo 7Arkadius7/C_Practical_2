75e5feadbca5230ac1148e3caca9f27b818eaeff
Sergey <motor-person@yandex.ru>
2022-10-03 21:05:26 +0400

    Сделали Task_14 и 16

---
+++ Task_14/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Пожалуйста, введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+// if (num % 7 == 0 && num % 23 == 0) Console.WriteLine($"{num} кратно");
+// else Console.WriteLine($"{num} не кратно");
+
+bool Multiplicity(int arg1)
+    {
+        return arg1 % 7 == 0 && arg1 % 23 == 0;
+    }
+
+if (Multiplicity(num)) Console.WriteLine("Кратно!!!");
+else Console.WriteLine("Не кратно!!!");
---
+++ Task_16/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool IsAliqut(int arg1, int arg2)
+{
+    return arg1 * arg1 == arg2 || arg2 * arg2 == arg1;
+}
+
+if (IsAliqut (num1, num2)) Console.WriteLine($"{num1}, {num2} => Да");
+else Console.WriteLine($"{num1}, {num2} => Нет");
